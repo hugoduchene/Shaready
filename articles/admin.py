c1bfee9370817ea7e817aa4bfe17bdc271cd8e93
Hugo Duchene <duchenehugo10@gmail.com>
2020-08-08 13:03:52 +0100

    [ADD] admin features

--- articles/admin.py
+++ articles/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
-
+from articles.models import (
+    Article,
+    Categories,
+    LikeArticle,
+    Comment,
+    LikeComment,
+)
 # Register your models here.
+
+admin.site.register(LikeComment)
+admin.site.register(Comment)
+admin.site.register(LikeArticle)
+admin.site.register(Categories)
+admin.site.register(Article)
--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from user.models import CustomUser, Subscription, Notification
 
 # Register your models here.
+
+admin.site.register(CustomUser)
+admin.site.register(Subscription)
+admin.site.register(Notification)
