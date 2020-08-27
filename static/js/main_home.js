cf913aeea3a40058261acb0390ffa6f8052840aa
Hugo Duchene <duchenehugo10@gmail.com>
2020-08-27 10:11:35 +0100

    [ADD] cookie banner on home

---
+++ static/js/main_home.js
@@ -0,0 +1,7 @@
+
+/* manage click on banner cookie */
+
+document.querySelector('.btn_cookie').addEventListener('click', (e)=>{
+    const banner_cookie = document.querySelector('.container_banner_cookie')
+    banner_cookie.classList.add('invisible')
+})
