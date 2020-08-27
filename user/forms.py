cb1e936c4d8531123e585393d6692d91e7cf29d7
Hugo Duchene <duchenehugo10@gmail.com>
2020-08-27 10:12:43 +0100

    [UPDATE] fix display error

--- user/forms.py
+++ user/forms.py
@@ -9,12 +9,12 @@ class RegistrationForm(UserCreationForm):
     password1 = forms.CharField(
         label=_("Password"),
         strip=False,
-        widget=forms.PasswordInput(attrs={'autocomplete': 'new-password', 'class' : 'input_registrer', 'placeholder' : 'Password1'}),
+        widget=forms.PasswordInput(attrs={'autocomplete': 'new-password', 'class' : 'input_registrer', 'placeholder' : 'Password'}),
         help_text=password_validation.password_validators_help_text_html(),
     )
     password2 = forms.CharField(
         label=_("Password confirmation"),
-        widget=forms.PasswordInput(attrs={'autocomplete': 'new-password', 'class' : 'input_registrer', 'placeholder' : 'Password2'}),
+        widget=forms.PasswordInput(attrs={'autocomplete': 'new-password', 'class' : 'input_registrer', 'placeholder' : 'Confirm password'}),
         strip=False,
         help_text=_("Enter the same password as before, for verification."),
     )
