8edcdc99816ea5ef36820de7ea783fc44cc45f60
Hugo Duchene <duchenehugo10@gmail.com>
2020-08-31 09:45:44 +0100

    [UPDATE] fix syntax error in settings

--- Shaready/settings/__init__.py
+++ Shaready/settings/__init__.py
@@ -246,8 +246,9 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.SessionAuthentication',
     ],
 
-     'DEFAULT_AUTHENTICATION_CLASSES': (
-            'rest_framework.authentication.BasicAuthentication',
-            'rest_framework.authentication.SessionAuthentication',
-        )
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    )
+}
 
